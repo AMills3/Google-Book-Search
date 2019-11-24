20db939e88d7e26da49bb6853a299a295a6ec693
Adam Miller <miad0801@gmail.com>
2019-11-24 07:42:34 -0600

    Beginning server

---
+++ server.js
@@ -0,0 +1,24 @@
+let express = require("express");
+let app = express();
+let routes = require(./routes);
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Connecting to Mongodb
+let mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+};
+
+// Adding routes
+app.use(routes);
+
+// Starting the server
+let PORT = process.env.PORT || 3001
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
