3096bc41ddec336600f0e49248ff2d5d34b39af0
Adam Miller <miad0801@gmail.com>
2019-11-24 08:31:20 -0600

    Building the api route

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+let path = require("path");
+let router = require("express").Router();
+let apiRoutes = require("./api");
+
+// Here is the API route
+
+router.use("/api, apiRoutes");
+
+// Send the react app
+
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
