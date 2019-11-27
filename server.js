5dfd2ed271983a598cddc2d5cb35a507d1380305
Adam Miller <miad0801@gmail.com>
2019-11-27 11:18:06 -0600

    Added html page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 let mongoose = require("mongoose");
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("client/public"));
 };
 
 // Adding routes
