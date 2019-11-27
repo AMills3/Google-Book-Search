bb71789bc358c1049d9374f089c9bbc6ba80c8a9
Adam Miller <miad0801@gmail.com>
2019-11-26 19:33:06 -0600

    Added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+// Create footer
+
+let Footer = () => {
+    return(
+        <footer className="footer navbar-fixed-bottom has-background-white text-center heading-subtitle">
+
+        Made with React
+
+        </footer>
+    );
+};
+
+export default Footer;
